85eccaf47af6a931b4d82697ef2e23d28e979ef3
Krupesh <krups224@gmail.com>
2023-12-27 19:47:26 +0530

    sidebar done

--- src/main/java/com/krupesh/smartcontactmanager/controllers/UserController.java
+++ src/main/java/com/krupesh/smartcontactmanager/controllers/UserController.java
@@ -1,5 +1,8 @@
 package com.krupesh.smartcontactmanager.controllers;
 
+import com.krupesh.smartcontactmanager.dao.UserRepository;
+import com.krupesh.smartcontactmanager.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +14,13 @@ import java.security.Principal;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/profile")
     public String profile(Model model, Principal principal) {
-        String userName = principal.getName();
+        User user = userRepository.findUserByEmail(principal.getName());
+        model.addAttribute("user", user);
         return "normal/user_profile";
     }
 
