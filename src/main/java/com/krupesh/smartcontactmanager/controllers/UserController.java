b629eac1697617144e889bfaef12b327a3c9dcb3
Krupesh <krups224@gmail.com>
2023-12-28 13:52:41 +0530

    add contact form done

--- src/main/java/com/krupesh/smartcontactmanager/controllers/UserController.java
+++ src/main/java/com/krupesh/smartcontactmanager/controllers/UserController.java
@@ -1,11 +1,13 @@
 package com.krupesh.smartcontactmanager.controllers;
 
 import com.krupesh.smartcontactmanager.dao.UserRepository;
+import com.krupesh.smartcontactmanager.entities.Contact;
 import com.krupesh.smartcontactmanager.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -17,11 +19,23 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/profile")
-    public String profile(Model model, Principal principal) {
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal) {
         User user = userRepository.findUserByEmail(principal.getName());
         model.addAttribute("user", user);
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model model, Principal principal) {
+        model.addAttribute("title", "View Your Profile");
         return "normal/user_profile";
+    }
+
+    @GetMapping("/add-contact")
+    public String addContact(Model model) {
+        model.addAttribute("title", "Add Contacts");
+        model.addAttribute("contact", new Contact());
+        return "normal/add_contact_form";
     }
 
 }
